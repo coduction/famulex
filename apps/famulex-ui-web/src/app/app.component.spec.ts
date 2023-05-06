d4b19d3b1190f3dd5e64bb91b96d3d89a9c167e5
Alexander Boeckle <boeckle@coduction.com>
2023-05-06 13:33:12 +0200

    #1 Remove tests from app.component.spec.ts since it is only a shell

--- apps/famulex-ui-web/src/app/app.component.spec.ts
+++ apps/famulex-ui-web/src/app/app.component.spec.ts
@@ -16,18 +16,18 @@ describe("AppComponent", () => {
     }).compileComponents();
   });
 
-  it("should render title", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Welcome famulex-ui-web"
-    );
-  });
-
-  it(`should have as title 'famulex-ui-web'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("famulex-ui-web");
-  });
+  // it("should render title", () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector("h1")?.textContent).toContain(
+  //     "Welcome famulex-ui-web"
+  //   );
+  // });
+  //
+  // it(`should have as title 'famulex-ui-web'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual("famulex-ui-web");
+  // });
 });
