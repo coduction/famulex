6d6407f3667fd3aed65d4ec7fbb9da2cd6d6ae3e
Alexander Boeckle <boeckle@coduction.com>
2023-05-06 13:25:36 +0200

    #1 Add ripple effect, layout preview

--- apps/famulex-ui-web/src/app/app.component.ts
+++ apps/famulex-ui-web/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component }          from "@angular/core";
+import { RouterModule }       from "@angular/router";
+import { PrimeNGConfig }      from "@coduction/primeng/api";
+import { NxWelcomeComponent } from "./nx-welcome.component";
 
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
-  selector: 'famulex-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "famulex-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'famulex-ui-web';
+  title = "famulex-ui-web";
+
+  constructor(primeConfig: PrimeNGConfig) {
+    primeConfig.ripple = true;
+  }
 }
