24c243437c47620790e02eacff54a4a6e50e8920
Alexander Boeckle <boeckle@coduction.com>
2023-05-08 18:30:37 +0200

    #1 NGRX demo testing

--- libs/ngrx-test/src/lib/ngrx-test/ngrx-test.component.spec.ts
+++ libs/ngrx-test/src/lib/ngrx-test/ngrx-test.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { NgrxTestComponent } from "./ngrx-test.component";
+import { NgrxTestFacade }            from "@famulex/ngrx-test";
+import { provideMockStore }          from "@ngrx/store/testing";
+import { NgrxTestComponent }         from "./ngrx-test.component";
 
 describe("NgrxTestComponent", () => {
   let component: NgrxTestComponent;
@@ -8,6 +10,10 @@ describe("NgrxTestComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NgrxTestComponent],
+      providers: [
+        provideMockStore(),
+        NgrxTestFacade
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NgrxTestComponent);
